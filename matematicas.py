d9b7bb21ed0631f28a795fb5c967828e49b9a4b7
brixton3 <brixton3@hotmail.com>
2024-10-12 17:05:01 -0300

    mas de modulo math

--- matematicas.py
+++ matematicas.py
@@ -6,3 +6,23 @@ def area(radio):
 
 
 print (area(10))
+
+# otras funciones
+
+# seno y cos
+print (math.sin(0.00))
+print (math.sin(math.pi))
+print (math.cos(0.00))
+print (math.sin(-1.59))
+print (math.cos(-1.59))
+print (math.cos(math.pi/2))
+print (math.sin(math.radians(30)))
+print (math.cos(math.radians(90)))
+# tangente
+
+print (math.tan(0.00))
+
+# math constantes
+
+print (math.e)
+print (math.tau)
