0975268e43d10cc6bc35dacfba0fb04fda59be2f
brixton3 <brixton3@hotmail.com>
2024-10-12 18:13:40 -0300

    ejercicios de strings y booleans

---
+++ strings_and_booleans.py
@@ -0,0 +1,28 @@
+# strings
+
+x = 'Welcome'
+print(x[3])
+
+# slicing strings
+x = 'Welcome'
+print(x[3:5])
+
+txt = 'Hello World'
+# una manera
+print(txt[2:5])
+
+# otra manera
+print (txt[3:])
+
+# booleans
+
+print(10 > 9)
+
+print(10 == 9)
+
+print(10 < 9)
+
+print(bool("abc"))
+
+
+print(bool(0))
