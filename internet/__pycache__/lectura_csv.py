a72d1b419b283a571b853a0db9333a53390e5c28
Brixton3 <Brixton3@hotmail.com>
2024-10-30 22:55:53 -0300

    actualizado

--- internet/__pycache__/lectura_csv.py
+++ internet/__pycache__/lectura_csv.py
@@ -4,10 +4,12 @@ import csv
 def read_csv_data():
     file_dir = os.path.dirname(os.path.abspath(__file__))
     csv_file = f"{file_dir}/subs.csv"
-
+    data = []
     with open(csv_file, mode="r") as file:
         reader = csv.DictReader(file) # uso de la funcion dictreader que crea un diccionario del csv q le pasemos
         for row in reader:
-            print(row)
+            if row["id"] == "2":
+                data.append(row)
+    return data
 
-read_csv_data()
+print(read_csv_data())
