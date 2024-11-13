c231b6f2db813b32af89bc886761630df37a0bc0
Brixton3 <Brixton3@hotmail.com>
2024-11-12 23:04:50 -0300

    cambios

--- lectura_csv.py
+++ lectura_csv.py
@@ -6,7 +6,7 @@ def read_csv_data():
     csv_file = f"{file_dir}/subs.csv"
     data = []
     with open(csv_file, mode="r") as file:
-        reader = csv.DictReader(file) # uso de la funcion dictreader que crea un diccionario del csv q le pasemos.
+        reader = csv.DictReader(file) # uso de la funcion dictreader que crea un diccionario del csv q le pasemos...
         for row in reader:
             if row["status"] == "activo":
                 data.append(row)
